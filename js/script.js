76b14fe399e8801d8206a9ed2a36966644103fd7
dries11 <daniel.ries@ymail.com>
2017-01-19 21:19:45 -0500

    Finished 1-6

--- js/script.js
+++ js/script.js
@@ -2,6 +2,55 @@
 
 var display = document.getElementById("display");
 
-function yourFunctionName (){
-  display.innerHTML = "hello";
+var n ;
+var random = 0;
+var printOut = 0;
+var o;
+
+var exerciseOne = function(){
+    for (var i = 0; i <= 10; i++) {
+        display.innerHTML += i + "<br>";
+    }
 }
+
+var exerciseTwo = function(){
+  display.innerHTML = "";
+  for(var i = 0; i <= 20; i++){
+    if((i%2 != 0)){
+      display.innerHTML += i + "<br>";
+    }
+  }
+}
+
+var exerciseThree = function(){
+  display.innerHTML = "";
+  for(var i = 1; i <= 10; i++){
+    display.innerHTML += (i*i) + "<br>";
+  }
+}
+    
+var exerciseFour = function(){
+  display.innerHTML = "";
+  for(var i = 0; i < 4; i ++){
+    random = (Math.ceil((Math.random()*10)));
+    display.innerHTML += (random) + "<br>";
+  }
+}
+    
+var exerciseFive = function(n){
+  display.innerHTML = "";
+  for(var i = 0; i < n; i ++){
+    if((i%2 == 0)){
+      display.innerHTML += (i) + "<br>";
+    }
+  }
+}
+    
+var exerciseSix = function(o){
+  display.innerHTML = "";
+    for(var o = 0; i < o; o++){
+      printOut = (Math.pow(2, o));
+      display.innerHTML += (printOut) + "<br>";
+    }
+}
+
